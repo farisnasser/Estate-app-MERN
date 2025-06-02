6bdd16cd8ba90ee769fc331ae84697650d5a6287
farisnasser <146169637+farisnasser@users.noreply.github.com>
2025-06-02 15:53:47 +0400

    create and run the server

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'   
+import Profile from './pages/Profile.jsx'
+import Header from './components/Header.jsx'  
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app" style={{ minHeight: '100vh', backgroundColor: '#f0f0f0' }}>
+        <Header />
+        <main style={{ padding: '20px' }}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/sign-in' element={<Signin />} />
+            <Route path='/sign-up' element={<SignUp />} />
+            <Route path='/profile' element={<Profile />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
